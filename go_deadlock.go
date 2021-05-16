d15a21aa0fecdfbcb55a3b6b659ffabb7d69c5e1
Anwesh43 <anweshthecool0@gmail.com>
2021-05-17 02:00:06 +0530

    correcting code for deadlock

--- go_deadlock.go
+++ go_deadlock.go
@@ -7,14 +7,14 @@ func main() {
 	ch2 := make(chan int)
 	ch3 := make(chan int)
 	go func() {
-		fmt.Println(<-ch2)
 		ch1 <- 10
+		fmt.Println(<-ch2)
+		ch3 <- 1
 	}()
 
 	go func() {
 		fmt.Println(<-ch1)
 		ch2 <- 20
-		ch3 <- 1
 	}()
 	<- ch3
 }
